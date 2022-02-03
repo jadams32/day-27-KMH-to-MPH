b5b2225b1a9f3184937ac29cad5d2afa1db0620a
John Adams Jr <john.c.adams32@gmail.com>
2022-02-02 19:10:34 -0700

    converison working, needs comments, and centering of elements

--- main.py
+++ main.py
@@ -3,32 +3,36 @@
 from tkinter import *
 FONT = ("Arial", 20, "normal")
 window = Tk()
-window.title("KMH to MPH")
-window.minsize(width=500, height=300)
+window.title("KMH to MPH Converter")
+window.minsize(width=350, height=350)
+window.config(padx=20, pady=20)
 
 
-def convert(self):
-    pass
+def convert():
+    user_number = input_field.get()
+    mph_value = round(int(user_number) * 0.62137119224)
+    conversion_display.config(text=f"{mph_value}")
 
 
-button = Button(text="Convert")
-button.grid(column=1, row=3)
+convert_button = Button(text="Convert", command=convert)
+convert_button.grid(column=1, row=2)
+
+input_field = Entry(width=8)
+input_field.grid(column=1, row=0)
 
 
-my_label = Label(text="is equal to", font=FONT)
-my_label.grid(column=0, row=2)
-my_label.config(padx=25, pady=25)
+is_equal_label = Label(text="is equal to", font=FONT)
+is_equal_label.grid(column=0, row=1)
 
-my_label = Label(text="0", font=FONT)
-my_label.grid(column=1, row=2)
-my_label.config(padx=25, pady=25)
+conversion_display = Label(text="0", font=FONT)
+conversion_display.grid(column=1, row=1)
 
-my_label = Label(text="Miles", font=FONT)
-my_label.grid(column=3, row=1)
-my_label.config(padx=25, pady=25)
+kilometers_per_hour = Label(text="KPH", font=FONT)
+kilometers_per_hour.grid(column=3, row=0)
 
-my_label = Label(text="KMH", font=FONT)
-my_label.grid(column=3, row=2)
-my_label.config(padx=25, pady=25)
+miles_per_hour = Label(text="MPH", font=FONT)
+miles_per_hour.grid(column=3, row=1)
+
+
 
 window.mainloop()
